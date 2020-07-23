2b1c8e71d80617aa313d8d4466d6b392b10322fa
unknown <jethanhargrave@gmail.com>
2020-07-23 16:01:33 -0400

    day3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,10 +1,13 @@
 """CPU functionality."""
 
 import sys
-LDI = 0b10000010  # LDI R0,8
-PRN = 0b01000111  # PRN R0
-HLT = 0b00000001  # HLT
-MUL = 0b10100010  # MUL R0,R1
+LDI = 0b10000010 
+PRN = 0b01000111 
+HLT = 0b00000001 
+MUL = 0b10100010 
+PUSH = 0b01000101
+POP = 0b01000110 
+SP = 7
 
 
 class CPU:
@@ -12,10 +15,55 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256 
+        self.ram = [0] * 256
         self.pc = 0 
-        self.running = True  
+        self.running = True
         self.reg = [0] * 8
+        self.reg[SP] = 0xf4
+        self.branch_table = {
+            LDI: self.ldi,
+            PRN: self.prn,
+            HLT: self.hlt,
+            MUL: self.mul,
+            PUSH: self.push,
+            POP: self.pop,
+        }
+
+    def mul(self):
+        reg_a = self.ram_read(self.pc + 1)
+        reg_b = self.ram_read(self.pc + 2)
+        self.alu("MUL", reg_a, reg_b)
+        self.pc += 3
+
+    def hlt(self):
+        self.pc += 1
+        self.running = False
+
+    def prn(self):
+        reg_num = self.ram_read(self.pc + 1)
+        print("PRN:", self.reg_read(reg_num))
+        self.pc += 2
+
+    def ldi(self):
+        reg_num = self.ram_read(self.pc + 1)
+        reg_val = self.ram_read(self.pc + 2)
+        self.reg_write(reg_num, reg_val)
+        self.pc += 3
+
+    def push(self):
+        self.reg[SP] -= 1
+        reg_num = self.ram[self.pc + 1]
+        value = self.reg[reg_num]
+        top_of_stack_addr = self.reg[SP]
+        self.ram[top_of_stack_addr] = value
+        self.pc += 2
+
+    def pop(self):
+        top_stack_val = self.reg[SP]
+        reg_addr = self.ram[self.pc + 1]
+        self.reg[reg_addr] = self.ram[self.reg[SP]]
+        self.reg[SP] += 1
+        self.pc += 2
 
     def load(self, filename):
         """Load a program into memory."""
@@ -25,8 +73,9 @@ class CPU:
         with open(filename) as f:
             for line in f:
                 line = line.split('#')
+
                 try:
-                    v = int(line[0], 2) 
+                    v = int(line[0], 2)
                 except ValueError:
                     continue
 
@@ -34,8 +83,7 @@ class CPU:
 
                 address += 1
 
-        # # For now, we've just hardcoded a program:
-
+        # hardcoded program:
         # program = [
         #     # From print8.ls8
         #     0b10000010,  # LDI R0,8
@@ -45,7 +93,6 @@ class CPU:
         #     0b00000000,
         #     0b00000001,  # HLT
         # ]
-
         # for instruction in program:
         #     self.ram[address] = instruction
         #     address += 1
@@ -81,46 +128,25 @@ class CPU:
         print()
 
     def ram_read(self, mar):
-        mar_reg = self.ram[mar] 
+        mar_reg = self.ram[mar]
         return mar_reg
 
     def ram_write(self, mar, mdr):
-        self.ram[mar] = mdr  
+        self.ram[mar] = mdr 
 
     def reg_read(self, mar):
-        mar_reg = self.reg[mar] 
+        mar_reg = self.reg[mar]  
         return mar_reg
 
     def reg_write(self, mar, mdr):
-        self.reg[mar] = mdr  
-
+        self.reg[mar] = mdr
     def run(self):
-        """Run the CPU."""
         while self.running:
             ir = self.ram[self.pc]
 
-            if ir == LDI:
-                reg_num = self.ram_read(self.pc + 1)
-                reg_val = self.ram_read(self.pc + 2)
-                self.reg_write(reg_num, reg_val)
-                self.pc += 3
+            if ir in self.branch_table:
+                self.branch_table[ir]()
 
-
-            elif ir == PRN:
-                reg_num = self.ram_read(self.pc + 1)
-                print("PRN:", self.reg_read(reg_num))
-                self.pc += 2
-
-            elif ir == MUL:
-                reg_a = self.ram_read(self.pc + 1)
-                reg_b = self.ram_read(self.pc + 2)
-                self.alu("MUL", reg_a, reg_b)
-                self.pc += 3
-
-
-            elif ir == HLT:
-                self.running = False
-                self.pc += 1
             else:
                 print(f"Unknown expression {ir} at address {self.pc}")
                 sys.exit(1)
